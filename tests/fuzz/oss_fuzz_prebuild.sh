7cc9c9c5f4f37a2003d6d23fe8a692cd32884134
Hidde Beydals <hidde@hhh.computer>
2023-05-24 17:23:45 +0200

    Rename controllers to controller
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- tests/fuzz/oss_fuzz_prebuild.sh
+++ tests/fuzz/oss_fuzz_prebuild.sh
@@ -21,8 +21,8 @@ set -euxo pipefail
 
 # Some tests requires embedded resources. Embedding does not allow
 # for traversing into ascending dirs, therefore we copy those contents here:
-mkdir -p internal/controllers/testdata/crd
-cp config/crd/bases/*.yaml internal/controllers/testdata/crd
+mkdir -p internal/controller/testdata/crd
+cp config/crd/bases/*.yaml internal/controller/testdata/crd
 
 # Version of the source-controller from which to get the GitRepository CRD.
 # Pulls source-controller/api's version set in go.mod.
@@ -32,10 +32,10 @@ SOURCE_VER=$(go list -m github.com/fluxcd/source-controller/api | awk '{print $2
 # Pulls image-reflector-controller/api's version set in go.mod.
 REFLECTOR_VER=$(go list -m github.com/fluxcd/image-reflector-controller/api | awk '{print $2}')
 
-if [ -d "../../internal/controllers/testdata/crds" ]; then
-    cp ../../internal/controllers/testdata/crds/*.yaml testdata/crds
+if [ -d "../../internal/controller/testdata/crds" ]; then
+    cp ../../internal/controller/testdata/crds/*.yaml testdata/crds
 else
     # Fetch the CRDs if not present since we need them when running fuzz tests on CI.
-    curl -s --fail https://raw.githubusercontent.com/fluxcd/source-controller/${SOURCE_VER}/config/crd/bases/source.toolkit.fluxcd.io_gitrepositories.yaml -o internal/controllers/testdata/crd/gitrepositories.yaml
-    curl -s --fail https://raw.githubusercontent.com/fluxcd/image-reflector-controller/${REFLECTOR_VER}/config/crd/bases/image.toolkit.fluxcd.io_imagepolicies.yaml -o internal/controllers/testdata/crd/imagepolicies.yaml
+    curl -s --fail https://raw.githubusercontent.com/fluxcd/source-controller/${SOURCE_VER}/config/crd/bases/source.toolkit.fluxcd.io_gitrepositories.yaml -o internal/controller/testdata/crd/gitrepositories.yaml
+    curl -s --fail https://raw.githubusercontent.com/fluxcd/image-reflector-controller/${REFLECTOR_VER}/config/crd/bases/image.toolkit.fluxcd.io_imagepolicies.yaml -o internal/controller/testdata/crd/imagepolicies.yaml
 fi
