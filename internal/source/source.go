d73a33751eaf8f44f5e5f15d1ac1f8bc7f86cd72
kane8n <takumi.kaneda@zozo.com>
2025-06-10 19:57:21 +0900

    SparseCheckout when Spec.Update.Path specified
    Signed-off-by: kane8n <takumi.kaneda@zozo.com>

--- internal/source/source.go
+++ internal/source/source.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"text/template"
 	"time"
@@ -193,6 +194,14 @@ func WithCheckoutOptionLastObserved(commit string) CheckoutOption {
 func WithCheckoutOptionShallowClone() CheckoutOption {
 	return func(cc *repository.CloneConfig) {
 		cc.ShallowClone = true
+	}
+}
+
+// WithCheckoutOptionRecurseSubmodules is a CheckoutOption option to configure
+// SparseCheckoutDirectories.
+func WithCheckoutOptionSparseCheckoutDirectories(updatePath string) CheckoutOption {
+	return func(cc *repository.CloneConfig) {
+		cc.SparseCheckoutDirectories = []string{filepath.Clean(updatePath)}
 	}
 }
 
