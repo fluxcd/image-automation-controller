6b676f7e8107d29217a98b2a9c77f5de25a9534f
Matheus Pimenta <matheuscscp@gmail.com>
2026-06-15 11:30:12 +0100

    Remove deprecated APIs in group image.toolkit.fluxcd.io/v1beta2
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- api/v1beta2/doc.go
+++ api/v1beta2/doc.go
@@ -14,10 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1beta2 contains API types for the image API group, version
-// v1beta2. The types here are concerned with automated updates to
-// git, based on metadata from OCI image registries gathered by the
-// image-reflector-controller.
+// Package v1beta2 contains API types for the image v1beta2 API group
+//
+// Deprecated: v1beta2 is no longer supported, use v1 instead.
 //
 // +kubebuilder:object:generate=true
 // +groupName=image.toolkit.fluxcd.io
